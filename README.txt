852986ca17897a2332b277fb16cce1aa895b3ce3
Erick Santos <erick242.es@gmail.com>
2017-07-30 21:13:14 -0700

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,9 @@
+-Erick Santos	
+-(erick_santos@csu.fullerton.edu)
+-Language used C++
+-The Program must be executed through the terminal in a Linux based
+system. Compile using command (g++ then file name.cpp) if any error occur fix code (there should be no errors).
+-To run the program type (./a.out) thats the executeble file created by the system to run on the terminal.
+-The Execution time for both downloader are Serial (34 min, 8.937 s) Parallel(28 m, 31.860 s) for the urls given
+-No extra credit was implemented
+-No especial request to run program
---
+++ SerialD.cpp
@@ -0,0 +1,52 @@
+#include <sys/wait.h>
+#include <iostream>
+#include <cstring>
+#include <fstream>
+#include <cstdlib>
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+
+
+int main()
+{
+  ifstream urls;
+  char name[256];//creates an array if charaters
+  int number = 0;//keeps count of the number of urls
+  string file = "urls.txt";//automatically adds file 
+  pid_t pid = -1;//sets initial pid to null
+
+  cout << "Opening file -- <" << file <<">"<< endl << endl;//displays the file from which is getting urls
+  urls.open(file.c_str());//opens file containing urls
+	if (urls.fail()){//checks if the file was found or opened
+		cerr << "ERROR -- FILE NOT FOUND!" << endl;
+		exit(1);//if the file wasn't found exit process
+	}
+	else{
+		while (urls.getline(name, sizeof(name))){//gets every url and puts it into an array of strings
+			number++;//keeps counts of urls processed
+
+  			pid = fork();//creates a fork
+
+  			if (pid < 0){//checks for fork's failure
+				cerr << "Fork Failed";
+				exit(1);//exit if the fork failed
+  			}
+  			else if (pid == 0){//creates a child to download files
+				cout << "**URL " << name << " is currently downloading...**\n";
+				execlp("/usr/bin/wget", "wget", name, NULL);//the command to downlad the files
+			}
+			else{//parent waiting for child
+				wait(NULL);
+				cout << "**URL #" << number << " has been downloaded!**\n\n";
+			}
+		}
+	}
+  urls.close();//close the file 
+  return 0;//exit program
+}
+
+
+
